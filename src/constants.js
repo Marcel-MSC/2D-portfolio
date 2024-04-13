17c2d2b937b6e34e8ddbfaa17d7fcd3ba3701ad5
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-04-12 21:50:20 -0300

    edied dialogueData

--- src/constants.js
+++ src/constants.js
@@ -6,18 +6,17 @@ export const dialogueData = {
       Here is my <a href="https://github.com/Marcel-MSC" target="_blank">Github</a>!`,
     "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
+    Play some video-games, watch tv shows and movies, learn or read a book.`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+     <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free Code Camp</a>, <a href="https://www.youtube.com/@TEDx" target="_blank">TEDx</a>,
+    <a href="https://www.youtube.com/@Programadorbr" target="_blank">Programador BR</a> (sometimes) and <a href="https://www.youtube.com/@FilipeDeschamps" target="_blank">Felipe Deschamps</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     resume: `This is my desk and on it is my resume. <a href="https://marcelomsc-cv.netlify.app/" target="_blank">Check it out?</a>
     Contact me at marcelomarcos.s.c@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+    Kaboom.js by watching some of my tutorials <a href="https://www.youtube.com/watch?v=gwtfWORCN0U" target="_blank">here</a>.`,
+    library: `There are a lot of programming books on my shelves.
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+    exit: `If you want to exit portfolio, just close the tab.`,
   };
