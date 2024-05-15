4f1fef8d4e4ed66449bcd67511c6aa5f028a219b
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-05-15 13:18:57 -0300

    changed dialogData constants

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+      I've made a couple of apps in that some languages. I also like C#. Anyway regardless of the language, I just like programming.
       Here is my <a href="https://github.com/Marcel-MSC" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "cs-degree": `This is my future CS degree. I will hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
     Play some video-games, watch tv shows and movies, learn or read a book.`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
@@ -15,7 +15,7 @@ export const dialogueData = {
     Contact me at marcelomarcos.s.c@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://www.youtube.com/watch?v=gwtfWORCN0U" target="_blank">here</a>.`,
+    Kaboom.js by watching the tutorial <a href="https://www.youtube.com/watch?v=gwtfWORCN0U" target="_blank">here</a>.`,
     library: `There are a lot of programming books on my shelves.
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit portfolio, just close the tab.`,
